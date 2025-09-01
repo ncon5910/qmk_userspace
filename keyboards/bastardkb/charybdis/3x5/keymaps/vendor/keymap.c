39fe462eea6c74173138e7ed9d8b3de527eaaa9d
Nicholas Conner <nicholas.conner@weathergroup.com>
2025-09-01 17:35:56 -0400

    Potentially fixed Charybdis nano keymap

--- keyboards/bastardkb/charybdis/3x5/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/vendor/keymap.c
@@ -159,7 +159,7 @@ static uint16_t auto_pointer_layer_timer = 0;
                       KC_LPRN, KC_RPRN, KC_UNDS, _______, XXXXXXX
 
 /** \brief Layout for gaming with no homerow mods (3 rows, 10 columns). */
-#define LAYOUT_LAYER_Gaming                                                                     \
+#define LAYOUT_LAYER_GAMING                                                                     \
        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, \
        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_QUOT, \
        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, \
@@ -228,10 +228,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // --- Combo definitions for toggling GAMING layer ---
 // Combo: Z (KC_Z) + / (KC_SLSH)
+enum custom_keycodes {
+    CB_GAMING = SAFE_RANGE,
+};
+
 const uint16_t PROGMEM gaming_combo[] = {KC_Z, KC_SLSH, COMBO_END};
 
 combo_t key_combos[] = {
-    COMBO(gaming_combo, SAFE_RANGE),
+    COMBO(gaming_combo, CB_GAMING),
 };
 
 // Track GAMING layer toggle state
