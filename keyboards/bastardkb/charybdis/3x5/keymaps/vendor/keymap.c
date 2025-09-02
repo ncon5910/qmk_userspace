0b4f3aed43092e587794c2de70d46bd94b2d4468
Nicholas Conner <nicholas.conner@weathergroup.com>
2025-09-01 22:53:35 -0400

    Directly add LT(LAYER_POINTER, KC_Z) insteaed of using _POINTER_MOD

--- keyboards/bastardkb/charybdis/3x5/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/vendor/keymap.c
@@ -166,7 +166,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 #define LAYOUT_LAYER_GAMING                                                                     \
        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, \
        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_QUOT, \
-       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, \
+       LT(LAYER_POINTER, KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, LT(LAYER_POINTER, KC_SLSH), \
                       KC_ESC, KC_SPC, KC_TAB, KC_ENT, KC_BSPC
 
 
@@ -237,7 +237,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_NUMERAL] = LAYOUT_wrapper(LAYOUT_LAYER_NUMERAL),
   [LAYER_POINTER] = LAYOUT_wrapper(LAYOUT_LAYER_POINTER),
   [LAYER_SYMBOLS] = LAYOUT_wrapper(LAYOUT_LAYER_SYMBOLS),
-  [LAYER_GAMING] = LAYOUT_wrapper(POINTER_MOD(LAYOUT_LAYER_GAMING)),
+  [LAYER_GAMING] = LAYOUT_wrapper(LAYOUT_LAYER_GAMING),
 };
 
 // clang-format on
